89a1bb5e436eef6c613a14d292a946262f10792e
minicreeper0217 <hiiv8828@gmail.com>
2026-09-03 06:47:51 +0800

    Debug

--- config.py
+++ config.py
@@ -127,7 +127,7 @@ secret = os.environ["WEB_SECRET"]
 
 web_secret = os.environ["WEB_TOKEN"]
 
-domain = os.environ["MODBOT_DOMAIN"]
+domain = "modbot.dev"
 
 yuki_domain = domain = os.environ["YUKI_DOMAIN"]
 
